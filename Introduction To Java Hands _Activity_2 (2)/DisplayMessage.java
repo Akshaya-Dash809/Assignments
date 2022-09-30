d09cb0275ecd88ff111d09c67fcb67b948e42b17
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-09-30 12:26:34 +0530

    Add files via upload

---
+++ Introduction To Java Hands _Activity_2 (2)/DisplayMessage.java
@@ -0,0 +1,15 @@
+package Lab_30Sep;
+
+class DisplayMessage{
+	  void printMessage(){
+	   System.out.println("Hello My Message");
+	}
+	}  
+
+	class  MyProgram
+	  {
+	    public static void main(String arg[]){
+	    	DisplayMessage display=new DisplayMessage();
+		      display.printMessage();
+}
+	  }
